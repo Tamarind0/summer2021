b168a540e98d0517cdbd403370c27289ab369d1f
Andres Maturin <amaturin8749@sdsu.edu>
2021-05-22 21:59:28 -0700

    Kinda fixed the spawning of the platforms, now we need to limit their X spawning location.

--- Project1/Assets/Scripts/PlatformGeneration.cs
+++ Project1/Assets/Scripts/PlatformGeneration.cs
@@ -7,36 +7,49 @@ public class PlatformGeneration : MonoBehaviour
 {
 
     
-    public float RandomX = 3f;
+    public float randomX = 1f;
+  
 
-    [SerializeField]
-   private GameObject Platform;
+    [SerializeField]private GameObject Platform;
 
-   [SerializeField]
-   private Transform TopPoint;
-    [SerializeField]
-    private float distanceBetweenY;
+   [SerializeField]private Transform TopPoint;
 
-    public float minY = .2f;
-    public float maxY = 1.5f;
-  //  public LayerMask mask;
+    [SerializeField] private float distanceBetweenY;
+  
+    private float platformHeight;
+
+   // public float minY = .2f;
+   // public float maxY = 1.5f;
+   // public float levelWidth = 3f;
+
+//brackeys spawner no bueno
+  void Start(){
+    platformHeight = Platform.GetComponent<BoxCollider2D>().size.y;
+    
+   // Vector3 spawn  = new Vector3();
+   //     for(int i = 0; i < 4; i++){ 
+   //             spawn.y += Random.Range(minY, maxY);
+   //            spawn.x = Random.Range(-levelWidth, levelWidth);
+   //      Instantiate(Platform, spawn, Quaternion.identity);
+   //         }
+ 
+  }
     // Update is called once per frame
     void Update()
     {   
+
         if(transform.position.y < TopPoint.position.y){
-            for(int i = 0; i < 4; i++){ // creating 4 platforms
-                float randomY = Random.Range(minY, maxY);
-                float randomX = Random.Range(-RandomX, RandomX);
-              //  if(i < 3){
-             transform.position = new Vector3 (randomX, transform.position.y, transform.position.z);
+           for(int i = 0; i < 10; i++){ // creating 4 platforms
+       //         spawn.y += Random.Range(minY, maxY);
+              //  float randomX = Random.Range(-RandomX, RandomX);
+             float xPosition = Random.Range(-randomX, randomX);
+             transform.position = new Vector3 (transform.position.x + xPosition, transform.position.y + platformHeight + distanceBetweenY, transform.position.z);
             GameObject newPlatform =  Instantiate(Platform, transform.position, transform.rotation);
-          //  Debug.Log(mask.ToString());
-           //  if(Pain.GetComponent<EdgeCollider2D>().IsTouchingLayers(mask) == true)
-           //  {
-            //     Debug.Log("DESTROYED");
-           //      Destroy(Pain);
-           //  } 
-            //    }
+           // if(newPlatform.transform.position.x >= -7){
+          //    Destroy(newPlatform);
+          //  }
+           }
+         }
 
 
 /// I forgot why we did this
@@ -55,6 +68,6 @@ public class PlatformGeneration : MonoBehaviour
          //  randomX2 = Random.Range(-RandomX, RandomX);
          //   transform.position = new Vector3 (randomX2, transform.position.y + distanceBetween, transform.position.z);
          //   Instantiate(Platform, transform.position, transform.rotation);
-        }
-    }
+        
+    
 }
