ada092800f5030b9ece94cf2c2a31593163b12b0
Andres Maturin <amaturin8749@sdsu.edu>
2021-05-25 11:07:43 -0700

    Fixed the player death initiation.
    Made a lowerBound Gameobject that will be 5f below the player, and once the player passes that lowerBound they trigger the GameOver scene.

--- Project1/Assets/Scripts/PlayerDied.cs
+++ Project1/Assets/Scripts/PlayerDied.cs
@@ -7,12 +7,19 @@ public class PlayerDied : MonoBehaviour
 {
 
     [SerializeField] private GameObject _player;
+    [SerializeField] private float lowerBoundValue;
+    private float maxHeight = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if(_player.transform.position.y < transform.position.y){ //player fell and is now by the despawner
+        if(_player.transform.position.y > maxHeight){
+            maxHeight = _player.transform.position.y;
+            transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y - lowerBoundValue, _player.transform.position.z);
+        }
+        if(_player.transform.position.y < transform.position.y){
             SceneManager.LoadScene("GameOver");
         }
+        
     }
 }
