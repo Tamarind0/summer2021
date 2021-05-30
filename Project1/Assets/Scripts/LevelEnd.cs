ec5ddf10e37475a25af45e4f97089f51ea7071f7
Andres Maturin <idontknowwhattonamethiskhay@gmail.com>
2021-05-30 11:55:08 -0700

    Unfortunately I had to eliminate the LeaderBoard scene. Instead I modified the GameOver canvas.

--- Project1/Assets/Scripts/LevelEnd.cs
+++ Project1/Assets/Scripts/LevelEnd.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 using TMPro;
 public class LevelEnd : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI textBox;
-    [SerializeField]private TextMeshProUGUI errorBox;
-    [SerializeField] private TMP_InputField inputField;
-
-    private ScoreCounter _score;
-
+    [SerializeField] private TextMeshProUGUI scoreTextBox;
+    [SerializeField]private TextMeshProUGUI highScoreBox;
     [SerializeField] private Rigidbody2D rb;
+    private ScoreCounter _score;
 
     private TextMeshProUGUI otherCanvas;
 
@@ -18,36 +15,27 @@ public class LevelEnd : MonoBehaviour
         if(GameObject.Find("ScoreCounter") != null){
             _score = GameObject.Find("ScoreCounter").GetComponent<ScoreCounter>();
         }
-        errorBox.color = new Color32(255, 0, 0, 0);
-       // notEnoughText.color = new Color32(255, 255, 255, 255);
     }
 
     // Start is called before the first frame update
    public void Spawn(){
-      gameObject.SetActive(true); // appearing the canvas
+       gameObject.SetActive(true); // appearing the canvas
        Cursor.lockState = CursorLockMode.None;
        rb.constraints = RigidbodyConstraints2D.FreezeAll;
-       textBox.text = _score.heightScore.ToString();
-       inputField.characterLimit = 4;
+       if(_score.heightScore > PlayerPrefs.GetInt("HighScore")){
+       PlayerPrefs.SetInt("HighScore", _score.heightScore);
+        }
+       highScoreBox.text = "HighScore "+ PlayerPrefs.GetInt("HighScore");
+       scoreTextBox.text = _score.heightScore.ToString();
+
   
    }
 
-    public void PlayerNameInput(){
-        _score.playerName = inputField.text;
-        inputField.readOnly = true;
-        
-    }
 
     public void SavePlayerInfo(){
-        //not allowing the player to choose the same name
-       // if(_score.LeaderBoardInfo.ContainsKey(_score.playerName)){
-       //     Debug.Log("SAME NAME");
-        //    inputField.readOnly = false;
-       //     errorBox.color = new Color32(255, 0, 0, 255);
-       //     errorBox.text = "ERROR: This name already exists";
+        
+        PlayerPrefs.DeleteAll();
 
-       // }else{
-        _score.LeaderBoardInfo.Add(_score.playerName, _score.heightScore);
-      //  }
+ 
     }
 }
--- Project1/Assets/Scripts/ScoreCounter.cs
+++ Project1/Assets/Scripts/ScoreCounter.cs
@@ -6,10 +6,7 @@ public class ScoreCounter : MonoBehaviour
 {
     //singelton of the Players score
     public int heightScore;
-    public string playerName;
 
-     public Dictionary<string, int> LeaderBoardInfo = new Dictionary<string, int>();
-     //should store both the player name and height to be used in the leader board scene
     private static ScoreCounter _score;
     public static ScoreCounter score
     {
@@ -28,13 +25,13 @@ public class ScoreCounter : MonoBehaviour
         if(_score != null)
         {
             Destroy(this);
-            Debug.Log("Destroyed");
+            
         }
         else
         {
             _score = this;
             DontDestroyOnLoad(this);
-            Debug.Log("Not destroyed");
+           
         }
     }
 
